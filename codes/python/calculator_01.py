7c5211276e0723e418e0c9d32d70f550e5c5bb2a
yokino-dev <lannyke7@gmail.com>
2025-07-12 19:31:00 -0300

    Adicionando o comentário no código, de seu uso

--- codes/python/calculator_01.py
+++ codes/python/calculator_01.py
@@ -1,6 +1,16 @@
 import os
 import time
 
+# Esse pequeno código em Python irá fazer as 4 operações básica da matemática:
+# Somar, Subtrair, Multiplicação, Divisão
+# O intuito principal desse conceito código são para utilizar as funções (def)
+# e como são aplicadas dentro do código.
+# pretendo melhorar futuramente pra torna-lo uma calculadora cientifica de facil uso
+# no momento apenas diretamente pelo terminal, depois aplicando interface grafica, começando pela
+# mais simples, Tkinter, nativa do Python.
+
+# Editor de Código: helix - Konsole
+
 def limpar_tela():
     if os.name == 'nt':
         os.system('cls')  # Windows
